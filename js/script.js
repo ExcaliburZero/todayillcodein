9d90a24eee722fd1a87784d16587035c8f9f2e79
ExcaliburZero <cwellsny@nycap.rr.com>
2015-07-02 20:30:09 -0400

    Set languages to have different chances to appear
    Separated the languages into different arrays depending on how commonly known they seem to be. Set it so that one of the lists is chosen and then a random language from the selected list is chosen, so that the more commonly known languages appear more often.
    
    Closes #4

--- js/script.js
+++ js/script.js
@@ -1,23 +1,13 @@
-// Array that holds the names of all of the possible computer languages that can appear on the page
-var languages = [
-    "Actionscript",
-    "Ada",
+// Arrays that hold the names of all of the possible computer languages that can appear on the page
+
+// Common languages
+var languages_common = [
     "Assembly",
     "Bash",
     "BASIC",
-    "Binary",
-    "Brainfuck",
     "C",
     "C++",
     "C#",
-    "Caml",
-    "COBOL",
-    "ColdFusion",
-    "Curl",
-    "D",
-    "DIBOL",
-    "Erlang",
-    "Forth",
     "Fortran",
     "Haskell",
     "Java",
@@ -25,29 +15,76 @@ var languages = [
     "Lisp",
     "Lua",
     "Objective-C",
-    "OCaml",
     "Pascal",
     "Perl",
     "PHP",
     "Python",
     "Ruby",
-    "Rust",
-    "Scala",
-    "Smalltalk",
     "SQL",
     "Swift",
-    "Tcl",
     "Visual Basic"
 ];
 
-// Pick a random computer language
-function randomLang() {
-    return Math.floor((Math.random() * languages.length));
+// Uncommon languages
+var languages_uncommon = [
+    "Actionscript",
+    "Ada",
+    "Caml",
+    "COBOL",
+    "ColdFusion",
+    "Curl",
+    "D",
+    "DIBOL",
+    "OCaml",
+    "Rust",
+    "Scala",
+    "Tcl"
+];
+
+// Strange languages
+var languages_strange = [
+    "Binary",
+    "Brainfuck",
+    "Erlang",
+    "Forth",
+    "Smalltalk"
+];
+
+// Pick a random computer language from the list given as an argument
+function randomLang(language_list) {
+    return Math.floor(Math.random() * language_list.length);
+}
+
+// Pick a random list of computer languages
+function randomList() {
+    // Set the chance for each list to be chosen
+    var common_range = 84;
+    var uncommon_range = 35;
+    var strange_range = 20;
+    var total_range = common_range + uncommon_range + strange_range;
+
+    // Randomly choose the list of languages to choose a random language from
+    language_list_num = Math.floor((Math.random() * total_range) + 1);
+    var language_list;
+
+    if (language_list_num < common_range) {
+        language_list = languages_common.slice(0);
+    } else if (language_list_num < (uncommon_range + common_range)) {
+        language_list = languages_uncommon.slice(0);
+    } else {
+        language_list = languages_strange.slice(0);
+    }
+
+    return language_list.slice(0);
 }
 
 // Put a random computer language name into the page
 function setRandomLang() {
-    $('.random-language').html(languages[randomLang()]);
+    // Pick a random list of computer languages
+    var language_list = randomList();
+
+    // Put the random computer language name into the page
+    $('.random-language').html(language_list[randomLang(language_list)]);
     return;
 }
 
